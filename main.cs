7e4ccace365480f1142e7d0efe1b0fdb3d85d7c4
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-11 00:01:07 +0000

    changes in main.cs

--- main.cs
+++ main.cs
@@ -1,7 +1,56 @@
+// Created by: Kenny Le
+// Created on: May 2022
+//
+// This program calculates the value of π
+
 using System;
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+class Program
+{
+    public static void Main(string[] args)
+    {
+        // This function calculates the value of π
+        const int NUMERATOR = 4;
+        int userInput;
+        int counter = 0;
+        var answer = 0;
+        var denominator = 1;
+
+        // input
+        Console.WriteLine("This program calculates the value of π");
+        Console.WriteLine("Please input a positive whole number");
+
+        Console.WriteLine("");
+        Console.Write("Input the whole number: ");
+        userInput = Convert.ToInt32(Console.ReadLine());
+
+        // process
+        if (userInput < 0)
+        {
+            Console.WriteLine("Please input a positive whole number!");
+        }
+        else
+        {
+            while (counter < userInput);
+            {
+                if (counter % 2 == 0);
+                {
+                    denominator = counter * 2 - 1;
+                    answer += NUMERATOR / denominator;
+                } 
+                else 
+                {
+                    denominator = counter * 2 - 1;
+                    answer += - (NUMERATOR / denominator);
+                }
+
+                counter++;
+            }
+
+            // output
+            Console.WriteLine("The value of π is " + (answer));
+        }
+        
+        Console.WriteLine("\nDone.");
+    }
 }
